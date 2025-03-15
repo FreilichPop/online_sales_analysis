120f17849c8460d2eb58e2978d0f825ff5101031
Davor Popovic <popovicdavor84@gmail.com>
2025-03-15 11:59:38 +0100

    Saved local changes

--- main.py
+++ main.py
@@ -4,14 +4,10 @@ from product_manager import ProductManager
 if __name__ == "__main__":
     manager = ProductManager()
 
-    product1 = Product("Laptop", 1000, 5)
-    product2 = Product("Phone", 500, 10)
-    product3 = Product("Tablet", 300, 7)
+    product1 = Product("Laptop", 1000, 15)
+    product2 = Product("Phone", 500, 20)
+    product3 = Product("Tablet", 300, 10)
 
     manager.add_product(product1)
     manager.add_product(product2)
     manager.add_product(product3)
-
-    print("All Products: ")
-    manager.display_products()
-    print(f"Total Inventory Value: {manager.total_inventory_value()}")
